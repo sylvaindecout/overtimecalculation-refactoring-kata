fab43c820066db38ae171e9d97a6a017d29d5efb
Sylvain Decout <sylvain.decout+github@gmail.com>
2023-09-10 17:09:16 +0200

    :recycle: Remove temporary custom toString

--- src/main/kotlin/fr/sdecout/kata/overtimecalculation/Overtime.kt
+++ src/main/kotlin/fr/sdecout/kata/overtimecalculation/Overtime.kt
@@ -5,6 +5,4 @@ import kotlin.time.Duration
 data class Overtime(
     val hoursRate1: Duration,
     val hoursRate2: Duration,
-) {
-    override fun toString() = "Overtime(hoursRate1=${hoursRate1.inWholeHours}, hoursRate2=${hoursRate2.inWholeHours})"
-}
+)
