8c3142025e7f8373dc9e964a5bb35854fc2ecd8c
Sylvain Decout <sylvain.decout+github@gmail.com>
2023-09-10 17:08:13 +0200

    :recycle: Replace BigDecimal with Duration

--- src/main/kotlin/fr/sdecout/kata/overtimecalculation/CompensationCalculator.kt
+++ src/main/kotlin/fr/sdecout/kata/overtimecalculation/CompensationCalculator.kt
@@ -6,39 +6,40 @@ import kotlin.time.Duration.Companion.hours
 
 object CompensationCalculator {
 
-    val MAX_OVERTIME_HOURS_RATE_1 = BigDecimal.TEN
-    const val THRESHOLD_OVERTIME_HOURS_RATE_2 = 6
+    val MAX_OVERTIME_HOURS_RATE_1 = 10.hours
+    val THRESHOLD_OVERTIME_HOURS_RATE_2 = 6.hours
 
     fun calculateOvertime(hoursOvertimeTotal: BigDecimal, assignment: Assignment, briefing: Briefing): Overtime {
-        var hoursOvertimeRate1 = BigDecimal.ZERO
-        var hoursOvertimeRate2 = BigDecimal.ZERO
+        val overtimeTotalDuration = hoursOvertimeTotal.toLong().hours
+        var hoursOvertimeRate1 = Duration.ZERO
+        var hoursOvertimeRate2 = Duration.ZERO
         val isWatCodeUnion = briefing.watCode && assignment.isUnionized
         val isWatCodeNonUnionForeign = briefing.watCode && !assignment.isUnionized && briefing.foreign
         if (!briefing.watCode && !briefing.z3 && !assignment.isUnionized || briefing.hbmo && assignment.isUnionized || isWatCodeNonUnionForeign || isWatCodeUnion || briefing.foreign && !assignment.isUnionized) {
-            hoursOvertimeRate1 = hoursOvertimeTotal
+            hoursOvertimeRate1 = overtimeTotalDuration
         } else {
-            if (hoursOvertimeTotal.compareTo(BigDecimal.ZERO) < 1) {
+            if (overtimeTotalDuration <= Duration.ZERO) {
                 return Overtime(hoursOvertimeRate1, hoursOvertimeRate2)
-            } else if (hoursOvertimeTotal.compareTo(MAX_OVERTIME_HOURS_RATE_1) < 1) {
-                hoursOvertimeRate1 = hoursOvertimeTotal
+            } else if (overtimeTotalDuration <= MAX_OVERTIME_HOURS_RATE_1) {
+                hoursOvertimeRate1 = overtimeTotalDuration
             } else {
                 hoursOvertimeRate1 = MAX_OVERTIME_HOURS_RATE_1
-                hoursOvertimeRate2 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1)
+                hoursOvertimeRate2 = overtimeTotalDuration - MAX_OVERTIME_HOURS_RATE_1
                 if (assignment.isUnionized) {
-                    val threshold = calculateThreshold(assignment, THRESHOLD_OVERTIME_HOURS_RATE_2.toLong())
-                    hoursOvertimeRate2 = hoursOvertimeRate2.min(threshold)
+                    val threshold = calculateThreshold(assignment, THRESHOLD_OVERTIME_HOURS_RATE_2)
+                    hoursOvertimeRate2 = minOf(hoursOvertimeRate2, threshold)
                 }
             }
         }
         return Overtime(hoursOvertimeRate1, hoursOvertimeRate2)
     }
 
-    private fun calculateThreshold(assignment: Assignment, threshold: Long): BigDecimal {
-        val remainder: Duration = assignment.duration - threshold.hours
+    private fun calculateThreshold(assignment: Assignment, threshold: Duration): Duration {
+        val remainder: Duration = assignment.duration - threshold
         return if (remainder.isNegative()) {
-            BigDecimal.valueOf(assignment.duration.inWholeSeconds / 3600)
+            (assignment.duration.inWholeSeconds / 3600).hours
         } else {
-            BigDecimal.valueOf(threshold)
+            threshold
         }
     }
 
--- src/main/kotlin/fr/sdecout/kata/overtimecalculation/Overtime.kt
+++ src/main/kotlin/fr/sdecout/kata/overtimecalculation/Overtime.kt
@@ -1,8 +1,10 @@
 package fr.sdecout.kata.overtimecalculation
 
-import java.math.BigDecimal
+import kotlin.time.Duration
 
 data class Overtime(
-    val hoursRate1: BigDecimal,
-    val hoursRate2: BigDecimal,
-)
+    val hoursRate1: Duration,
+    val hoursRate2: Duration,
+) {
+    override fun toString() = "Overtime(hoursRate1=${hoursRate1.inWholeHours}, hoursRate2=${hoursRate2.inWholeHours})"
+}
