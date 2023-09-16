0df93df772bd4c9b8f01e1d7982aeba7d671b6e4
Sylvain Decout <sylvain.decout+github@gmail.com>
2023-09-16 15:05:02 +0200

    :white_check_mark: Add unit tests to CompensationCalculator

--- src/test/kotlin/fr/sdecout/kata/overtimecalculation/CompensationCalculatorTest.kt
+++ src/test/kotlin/fr/sdecout/kata/overtimecalculation/CompensationCalculatorTest.kt
@@ -2,11 +2,73 @@ package fr.sdecout.kata.overtimecalculation
 
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.property.assume
+import kotlin.time.Duration.Companion.hours
 
 class CompensationCalculatorTest : ShouldSpec({
 
-    should("FIXME") {
-        true shouldBe false
+    should("take all hours with rate 1 if briefing allows exceeding overtime limit in rate 1") {
+        val assignment = Assignment(isUnionized = false, duration = 1.hours)
+        val briefing = Briefing(watCode = false, z3 = false, foreign = false, hbmo = false)
+        assume(briefing.allowsExceedingOvertimeLimitInRate1(assignment.isUnionized))
+
+        val overtime = CompensationCalculator.calculateOvertime(12.hours, assignment, briefing)
+
+        overtime shouldBe Overtime(timeInRate1 = 12.hours)
+    }
+
+    should("take all hours with rate 1 if overtime duration is < 10h") {
+        val assignment = Assignment(isUnionized = false, duration = 1.hours)
+        val briefing = Briefing(watCode = false, z3 = true, foreign = false, hbmo = false)
+        assume(!briefing.allowsExceedingOvertimeLimitInRate1(assignment.isUnionized))
+
+        val overtime = CompensationCalculator.calculateOvertime(2.hours, assignment, briefing)
+
+        overtime shouldBe Overtime(timeInRate1 = 2.hours)
+    }
+
+    should("split hours between rate 1 and rate 2 if overtime duration is > 10h and assignment is not unionized") {
+        val overtimeTotalDuration = 12.hours
+        val assignment = Assignment(isUnionized = false, duration = 1.hours)
+        val briefing = Briefing(watCode = false, z3 = true, foreign = false, hbmo = false)
+        assume(!briefing.allowsExceedingOvertimeLimitInRate1(assignment.isUnionized))
+
+        val overtime = CompensationCalculator.calculateOvertime(overtimeTotalDuration, assignment, briefing)
+
+        overtime shouldBe Overtime(timeInRate1 = 10.hours, timeInRate2 = 2.hours)
+    }
+
+    should("split hours between rate 1 and rate 2 if overtime duration is > 10h and unionized assignment duration is > 6h") {
+        val overtimeTotalDuration = 12.hours
+        val assignment = Assignment(isUnionized = true, duration = 8.hours)
+        val briefing = Briefing(watCode = false, z3 = true, foreign = false, hbmo = false)
+        assume(!briefing.allowsExceedingOvertimeLimitInRate1(assignment.isUnionized))
+
+        val overtime = CompensationCalculator.calculateOvertime(overtimeTotalDuration, assignment, briefing)
+
+        overtime shouldBe Overtime(timeInRate1 = 10.hours, timeInRate2 = 2.hours)
+    }
+
+    should("split hours between rate 1 and rate 2 if overtime duration is > 10h and unionized assignment duration is < 6h") {
+        val overtimeTotalDuration = 12.hours
+        val assignment = Assignment(isUnionized = true, duration = 3.hours)
+        val briefing = Briefing(watCode = false, z3 = true, foreign = false, hbmo = false)
+        assume(!briefing.allowsExceedingOvertimeLimitInRate1(assignment.isUnionized))
+
+        val overtime = CompensationCalculator.calculateOvertime(overtimeTotalDuration, assignment, briefing)
+
+        overtime shouldBe Overtime(timeInRate1 = 10.hours, timeInRate2 = 2.hours)
+    }
+
+    should("cap hours at rate 2 to assignment duration if overtime duration is > 10h and unionized assignment duration is < 6h") {
+        val overtimeTotalDuration = 12.hours
+        val assignment = Assignment(isUnionized = true, duration = 1.hours)
+        val briefing = Briefing(watCode = false, z3 = true, foreign = false, hbmo = false)
+        assume(!briefing.allowsExceedingOvertimeLimitInRate1(assignment.isUnionized))
+
+        val overtime = CompensationCalculator.calculateOvertime(overtimeTotalDuration, assignment, briefing)
+
+        overtime shouldBe Overtime(timeInRate1 = 10.hours, timeInRate2 = 1.hours)
     }
 
 })
